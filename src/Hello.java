76e75ff365d92a5770309883c0252fde6f2796b4
anwr <anwr@live.se>
2020-11-17 03:18:49 +0100

    test21

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello guys, just a test!");
         System.out.println("Another test");
-        System.out.println("anther another test");
+        eadphifö
     }
-}ei
+}
