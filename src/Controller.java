79a5bfe7491abf70c6c641ef490765d01555b460
anwr <anwr@live.se>
2020-11-25 19:05:29 +0100

    implemented Meeting (schedule/remove/view)

--- src/Controller.java
+++ src/Controller.java
@@ -1,28 +1,32 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class Controller {
     public static void addProject() {
 
     }
-    public static void addTask(){
+
+    public static void addTask() {
 
     }
 
 
-
-    public static void showMemberList(){
+    public static void showMemberList() {
         for (Member member : Member.memberList) {
-            if (member != null){
+            if (member != null) {
                 System.out.println(member);
             }
         }
 
     }
-    public static void searchTM(){
+
+    public static void searchTM() {
 
     }
 
-    public static void addMember(){
+    public static void addMember() {
         String id = UUID.randomUUID().toString();
         String name = InputHandler.stringInput("Please enter member's name: ");
         String role = InputHandler.stringInput("Please enter member's role: ");
@@ -30,7 +34,7 @@ public class Controller {
         Member.registerMember(member);
         System.out.println(name + " was added successfully as a " + role + ". \n");
     }
-    public static void removeUser(String idInput){
+    /*public static void removeUser(String idInput){
         for (int i = 0; i < Member.memberList.size(); i++){
             if(Member.memberList.get(i).getId().equals(idInput)){
                 Member.memberList.remove(i);
@@ -38,26 +42,69 @@ public class Controller {
             } else System.out.println("Team member with id" + idInput + " not found");
         }
 
+    }*/
+
+
+    public static void removeTask() {
+
+    }
+
+
+    public static void assignTask() {
+
+    }
+
+    public static void viewProjects() {
+
+    }
+
+    public static void viewTasks() {
+
     }
 
 
+    public static void scheduleMeeting() {
+        String meetingId = UUID.randomUUID().toString();
+        String meetingDay = InputHandler.stringInput("Enter meeting date in form of dd-mm-yyyy");
+        String meetingTime = InputHandler.stringInput("Enter meeting time in form of hh:mm:ss");
+        String meetingD = meetingDay + " " + meetingTime;
+        Date meetingDate = null;
+        try {
+            meetingDate = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(meetingD);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String meetingAbout = InputHandler.stringInput("Please specify what the meeting is about in one word: ");
+        Meeting meeting = new Meeting(meetingId, meetingDate, meetingAbout);
+        Meeting.meetingList.add(meeting);
+        System.out.println("\t" + meetingDate + "\n " + meetingAbout + "\n");
+    }
 
-    public static void removeTask(){
-
+    public static void removeMember(String idInput) {
+        for (int i = 0; i < Member.memberList.size(); i++) {
+            if (Member.memberList.get(i).getId().equals(idInput)) {
+                Member.memberList.remove(i);
+                System.out.println("Member removed!");
+            } else System.out.println("Team member with id" + idInput + " not found");
+        }
     }
 
 
-    public static void assignTask(){
-
+    public static void showMeetingList() {
+        for (Meeting meeting : Meeting.meetingList) {
+            if (meeting != null) {
+                System.out.println(meeting);
+            }
+        }
     }
-    public static void viewProjects(){
 
-    }
-    public static void viewTasks(){
-
-    }
-    public static void scheduleMeeting(){
-
+    public static void removeMeeting(String meetingIdInput) {
+        for (int i = 0; i < Meeting.meetingList.size(); i++) {
+            if (Meeting.meetingList.get(i).getMeetingId().equals(meetingIdInput)) {
+                Meeting.meetingList.remove(i);
+                System.out.println("Meeting removed!");
+            } else System.out.println("Meeting with id" + meetingIdInput + " not found");
+        }
     }
 }
 
--- src/managerView.java
+++ src/managerView.java
@@ -1,4 +1,3 @@
-
 public class managerView {
 
 
@@ -8,12 +7,11 @@ public class managerView {
                 "2. Add a task\n" +
                 "3. Team members\n" +
                 "4. Remove a task\n" +
-                "5. Remove a team member\n" +
+                "5. View tasks\n" +
                 "6. Assign a task to a team member\n" +
                 "7. View all projects\n" +
-                "8. View tasks\n" +
-                "9. Schedule a meeting\n" +
-                "10. Return to Main Menu\n");
+                "8. Schedule a meeting\n" +
+                "9. Return to Main Menu\n");
         switch (managerMenuChoice) {
             case 1 -> Controller.addProject();
             case 2 -> Controller.addTask();
@@ -22,9 +20,24 @@ public class managerView {
             case 5 -> Controller.viewTasks();
             case 6 -> Controller.assignTask();
             case 7 -> Controller.viewProjects();
-            case 9 -> Controller.scheduleMeeting();
-            default -> Main.intro();
+            case 8-> Meetings();
+            default->{ }
         }
+    }
+
+    public static void Meetings(){
+        int mChoice = InputHandler.intInput("1. Schedule new meeting." +
+                "\n2. Remove a meeting." +
+                "\n3. View all scheduled meeting");
+        switch (mChoice) {
+            case 1 -> { Controller.scheduleMeeting();
+                Meetings(); }
+            case 2 -> { Controller.showMeetingList();
+            String id = InputHandler.stringInput("Which meeting should be removed? ID: ");
+            Controller.removeMeeting(id); Meetings();}
+            case 3 -> { Controller.showMeetingList(); Meetings(); }
+        }
+
     }
     public static void teamMembers() {
         int tmChoice = InputHandler.intInput("1. Add new team member." +
@@ -35,7 +48,7 @@ public class managerView {
             case 1 -> { Controller.addMember();teamMembers(); }
             case 2 -> { Controller.showMemberList();
                 String id = InputHandler.stringInput("Which member should be removed? ID: ");
-                Controller.removeUser(id); teamMembers();}
+                Controller.removeMember(id); teamMembers();}
             case 3 -> { Controller.showMemberList();teamMembers(); }
             case 4 -> Controller.searchTM();
             default -> { }
