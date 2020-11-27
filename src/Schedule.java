db66775aafd1915fc8d4fd6994554214ba7aef90
anwr <anwr@live.se>
2020-11-27 10:52:05 +0100

    changed from .text to .java

---
+++ src/Schedule.java
@@ -0,0 +1,93 @@
+package softManagement18;
+
+//import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
+abstract class Schedule {
+
+
+    private ArrayList<TeamMember> participants=new ArrayList<TeamMember>();
+    private String ID;
+    private Date date;
+    private String description;
+    private int startTime;
+    private int endTime;
+
+
+    // date(int year, int month, int date)
+    //public Schedule(String meetingId,Date meetingDate, String description,Time startTime,Time endTime)
+    public Schedule(String meetingId,Date meetingDate, String description,int startTime,int endTime) {
+        UUID uuid = UUID.randomUUID();
+        this.ID = uuid.toString();
+        this.ID = ID.substring(0, Math.min(ID.length(), 3));
+
+        this.date = meetingDate;
+        this.description = description;
+        this.startTime=startTime;
+        this.endTime=endTime;
+        this.participants=new ArrayList<TeamMember>();
+    }
+
+    ArrayList<TeamMember> getParticipants() {
+        return participants;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+
+    public String getID() {
+        return ID;
+    }
+
+
+    public Date getDate() {
+        return date;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public void listOfParticipants() {
+        if(this.participants.isEmpty()) {
+            System.out.println("\nThe list of participants is currently empty!");
+        }else {
+            for(TeamMember currentParticipant:participants) {
+                System.out.println(currentParticipant+"\n");
+            }
+        }
+    }
+
+    public String toString() {return "";};
+
+
+}
