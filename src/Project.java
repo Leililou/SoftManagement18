f0cfd5bf70c16b38a61deebbccc6ea8669a2be09
ramadi <ramadi@student.chalmers.se>
2020-11-27 10:40:46 +0100

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -1,11 +1,54 @@
+package softManagement18;
+
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Project {
-    private String id = UUID.randomUUID().toString();
-    private String name;
-    private String task;
 
 
+	private String ID;
+	private String title;
+	private String description;
+	
+	private static ArrayList<Task> tasks = new ArrayList<Task>();
+
+	public Project(String title, String description) {
+
+		UUID uuid = UUID.randomUUID();
+		this.ID = uuid.toString();
+		this.ID = ID.substring(0, Math.min(ID.length(), 3)); 
+
+		this.title=title;
+		this.description=description;
+	}
+
+	String getID() {
+		return ID;
+	}
+
+	String getTitle() {
+		return title;
+	}
+
+	String getDescription() {
+		return description;
+	}
+	
+	static ArrayList<Task> getTasks() {
+		return tasks;
+	}
+
+	void setTitle(String title) {
+		this.title = title;
+	}
+
+	void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String toString() {
+		return "ID: "+this.getID()+", Project's title:"+this.getTitle()+", Description:"+this.getDescription();
+	}
 
 
 
